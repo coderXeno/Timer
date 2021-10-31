ba6326ca1c96407094edafce23eac2c08bd67856
UniversalXeno <73885838+coderXeno@users.noreply.github.com>
2021-10-31 15:30:47 +0530

    wrote the js code for the timer

---
+++ index.js
@@ -0,0 +1,80 @@
+const pomodoroBtn = document.getElementById('pomodoroEl');
+const shortBtn = document.getElementById('shortEl');
+const longBtn = document.getElementById('longEl');
+const timerDisplay = document.getElementById('timer');
+const playPause = document.getElementById('status'); 
+const leftAnimation = document.querySelector(".left .fill")
+const rightAnimation = document.querySelector(".right .fill")
+let intervalFunction
+
+
+pomodoroBtn.addEventListener('click',()=>{
+    startTiming(25*60);
+})
+
+longBtn.addEventListener('click',()=>{
+    startTiming(15*60);
+})
+
+shortBtn.addEventListener('click',()=>{
+    startTiming(5*60);
+})
+playPause.addEventListener('click', ()=>{
+    clearInterval(intervalFunction)
+})
+
+
+function startTiming(time){
+    if(typeof intervalFunction !== undefined){
+        clearInterval(intervalFunction)
+    }
+    let allowedTime = time
+    showTime(allowedTime)
+    playPause.innerText = `pause`
+    animationstart(allowedTime)
+    intervalFunction = setInterval(()=>{
+        if(allowedTime == 0 ){
+            clearInterval(intervalFunction)
+            showTime(0)
+            playPause.innerText = `play`
+        }else{
+            allowedTime--
+            showTime(allowedTime)
+        }
+    },1000)
+}
+function showTime(allowedTime) {
+    let minutes = pad(Math.floor(allowedTime / 60 ))
+    let seconds = pad(allowedTime % 60)
+    
+    let time = `${minutes}:${seconds}`
+    document.title = time;
+    timerDisplay.innerText = time
+}
+
+function pad(number) {
+   return number < 10 ? `0${number.toString()}` : number
+}
+
+function animationstart(allowedTime) {
+    let milliSeconds = allowedTime * 1000 / 2
+    
+    leftAnimation.animate([
+        {transform: 'rotate(0deg)'},
+        {transform: 'rotate(180deg)'}
+    ],{
+        easing: "linear",
+        fill:"both",
+        duration: milliSeconds
+    })
+
+    rightAnimation.animate([
+        {transform: 'rotate(0deg)'},
+        {transform: 'rotate(180deg)'}
+    ],{
+        easing: "linear",
+        fill: "both",
+        duration: milliSeconds,
+        delay: milliSeconds
+    })
+}
